81ac95b33b3da019aa14166ff81012d7d3a4cae5
danbek2005 <danbek20508@gmail.com>
2019-10-03 21:49:35 +0300

    Updates

---
+++ static/js/6.f05ff453.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{340:function(e,n,s){e.exports={News:"News_News__3tjPk"}},346:function(e,n,s){"use strict";s.r(n);var t=s(0),w=s.n(t),a=s(340),c=s.n(a);n.default=function(){return w.a.createElement("div",{className:c.a.News},"News")}}}]);
+//# sourceMappingURL=6.f05ff453.chunk.js.map
