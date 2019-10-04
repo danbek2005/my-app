ad61b3e4d946a62092c0f41030140f8339603693
danbek2005 <danbek20508@gmail.com>
2019-10-04 21:33:10 +0300

    update url

--- src/Components/FeedBack/Feedback.js
+++ src/Components/FeedBack/Feedback.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import classes from './FeedBack.module.css';
+import Phone from './../../../public/files/Phone.png';
+import YouTube from './../../../public/files/YouTube.png';
+import Instagram from './../../../public/files/Instagram.png';
 var FeedBack = () => {
 	return (
 		<div className={classes.feedback}>
 			<div className={classes.feedback_item}>
-				<img className={classes.feedback_img} src="http://localhost:3000/files/Phone.png" alt="Phone"/>
+				<img className={classes.feedback_img} src={Phone} alt="Phone"/>
 			</div>
 			<div className={classes.feedback_item}>
-				<img className={classes.feedback_img} src="http://localhost:3000/files/YouTube.png" alt="YouTube"/>
+				<img className={classes.feedback_img} src={YouTube} alt="YouTube"/>
 			</div>
 			<div className={classes.feedback_item}>
-				<img className={classes.feedback_img} src="http://localhost:3000/files/Instagram.png" alt="Instagram"/>
+				<img className={classes.feedback_img} src={Instagram} alt="Instagram"/>
 			</div>
 		</div>
 	)
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Header.module.css';
 import LoginForm from './Login.jsx';
+import headerImg from './../../../public/files/header.jpg';
 var Header = (props) => {
 	return (
 		<div>
 			<div className={classes.header}>
-				<img className={classes.header_titleImg} src="http://localhost:3000/files/header.jpg"/>
+				<img className={classes.header_titleImg} src={headerImg}/>
 			</div>
 			<div className={classes.Auth}>
 				{props.getAuthElements(props, classes)}
